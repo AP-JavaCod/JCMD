af5f60aa359aae5fee3dad060ffbe62c4e30dd2b
AP-JavaCod <megafoh3g@gmail.com>
2025-07-02 22:19:05 +0500

    Create java_project.cpp

---
+++ system/compilation/java_project.cpp
@@ -0,0 +1,32 @@
+#include "java_project.h"
+
+java_project::java_project(std::filesystem::path nameProject, std::filesystem::path mainClass, double version) {
+	this->nameProject = nameProject;
+	this->mainClass = mainClass;
+	this->version = version;
+	parsJavaClass(nameProject);
+}
+
+void java_project::parsJavaClass(std::filesystem::path path) {
+	for (const auto& i : std::filesystem::directory_iterator(path)) {
+		std::filesystem::path fil = i.path();
+		if (std::filesystem::is_directory(fil)) {
+			parsJavaClass(fil);
+		}
+		else if (fil.extension() == ".java")
+		{
+			javaClasses.push_back(fil);
+		}
+	}
+}
+
+std::string getFormatPath(std::filesystem::path path, std::filesystem::path start) {
+	std::filesystem::path fil = std::filesystem::relative(path, start);
+	std::string name = fil.filename().string();
+	int sizeFormat = path.extension().string().size();
+	name.resize(name.size() - sizeFormat, ' ');
+	while ((fil = fil.parent_path()) != "") {
+		name = fil.filename().string() + "." + name;
+	}
+	return name;
+}
