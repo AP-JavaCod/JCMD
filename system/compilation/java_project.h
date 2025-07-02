f678f63d71f0daabfe0663ae1c6055bcf2f84480
AP-JavaCod <megafoh3g@gmail.com>
2025-07-02 22:18:00 +0500

    Create java_project.h

---
+++ system/compilation/java_project.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <filesystem>
+#include <string>
+
+struct java_project
+{
+	java_project(std::filesystem::path namePriject, std::filesystem::path, double version);
+
+	std::filesystem::path nameProject;
+	std::filesystem::path mainClass;
+	std::vector<std::filesystem::path> javaClasses;
+	double version;
+
+private:
+
+	void parsJavaClass(std::filesystem::path);
+
+};
+
+std::string getFormatPath(std::filesystem::path, std::filesystem::path);
