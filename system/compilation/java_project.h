0fe0b1c943bc66bd05cce7a747fb03e4f44f9aa7
AP-JavaCod <megafoh3g@gmail.com>
2025-07-03 08:57:14 +0500

    set bild file

--- system/compilation/java_project.h
+++ system/compilation/java_project.h
@@ -6,8 +6,10 @@
 struct java_project
 {
 	java_project(std::filesystem::path namePriject, std::filesystem::path, double version);
+	java_project(std::filesystem::path nameProject, std::filesystem::path, std::filesystem::path, double);
 
 	std::filesystem::path nameProject;
+	std::filesystem::path bild;
 	std::filesystem::path mainClass;
 	std::vector<std::filesystem::path> javaClasses;
 	double version;
