1160e6efa7d4e6bf056090fe75fe0ab756ab8ee5
AP-JavaCod <megafoh3g@gmail.com>
2025-07-05 07:43:16 +0500

    Update Builder.java

--- visualization/native/Builder.java
+++ visualization/native/Builder.java
@@ -1,11 +1,19 @@
 import java.io.File;
+import java.io.IOException;
 
 public class Builder {
 
     private static long data =-1;
 
-    public static void open(File file){
+    public static void open(File file) throws IOException {
+        if(isOpen()){
+            throw new IOException();
+        }
         data = setData(file.getPath());
+    }
+
+    public static boolean isOpen(){
+        return data != -1;
     }
 
     public static void close(){
@@ -23,9 +31,33 @@ public class Builder {
         return build(data, mainClass.getPath(), name, version);
     }
 
-    public static String ran(File mainClass, String name, double version){
+    public static String run(File mainClass, String name, double version){
        error();
-       return ran(data, mainClass.getPath(), name, version);
+       return run(data, mainClass.getPath(), name, version);
+    }
+
+    public static File[] getClasses(){
+        error();
+        int size = sizeClasses(data);
+        File[] classes = new File[size];
+        for (int i = 0; i < size; i++){
+            classes[i] = new File(getClassFile(data, i));
+        }
+        return classes;
+    }
+
+    public static String[] getClassesFormat(){
+        error();
+        int size = sizeClasses(data);
+        String[] classes = new String[size];
+        for (int i = 0; i < size; i++){
+            classes[i] = getClassFormat(data, i);
+        }
+        return classes;
+    }
+
+    public static File getProject(){
+        return new File(getProjectFile(data));
     }
 
     private static void error(){
@@ -35,10 +67,22 @@ public class Builder {
     }
 
     private static native long setData(String path);
+
     private static native int deleteData(long data);
+
     private static native String compilation(long data, String mainClass, double version);
+
     private static native String build(long data, String mainClass, String name, double version);
-    private static native  String ran(long data, String mainClass, String name, double version);
+
+    private static native  String run(long data, String mainClass, String name, double version);
+
+    private static native int sizeClasses(long data);
+
+    private static native String getClassFile(long data,int index);
+
+    private static native String getClassFormat(long data, int index);
+
+    private static native String getProjectFile(long data);
 
     static {
         System.loadLibrary("system/compilation/JICMD");
