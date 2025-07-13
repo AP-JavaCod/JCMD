b264e263753945fc9d861de467e19c4f78b5e65e
AP-JavaCod <megafoh3g@gmail.com>
2025-07-13 13:35:36 +0500

    Update builder_JVM.cpp

--- system/compilation/JCMD/builder_JVM.cpp
+++ system/compilation/JCMD/builder_JVM.cpp
@@ -50,6 +50,8 @@ JNIEXPORT jstring JNICALL Java_build_Builder_getClassFile(JNIEnv* env, jobject, 
 	return env->NewStringUTF(name.c_str());
 }
 
+
+//Deprecated
 JNIEXPORT jstring JNICALL Java_build_Builder_getClassFormat(JNIEnv* env, jobject, jlong data, jint index) {
 	java_project* project = reinterpret_cast<java_project*>(data);
 	std::filesystem::path path = project->javaClasses[index];
@@ -58,7 +60,15 @@ JNIEXPORT jstring JNICALL Java_build_Builder_getClassFormat(JNIEnv* env, jobject
 	return env->NewStringUTF(name.c_str());
 }
 
-JNIEXPORT jstring JNICALLJava_build_Builder_getProjectFile(JNIEnv* env, jobject, jlong data) {
+JNIEXPORT jstring JNICALL Java_build_Builder_getPathClass(JNIEnv* env, jobject, jlong data, jstring file) {
+	java_project* project = reinterpret_cast<java_project*>(data);
+	std::filesystem::path path = env->GetStringUTFChars(file, nullptr);
+	std::filesystem::path start = project->nameProject.string() + "\\src";
+	std::string name = getFormatPath(path, start);
+	return env->NewStringUTF(name.c_str());
+}
+
+JNIEXPORT jstring JNICALL Java_build_Builder_getProjectFile(JNIEnv* env, jobject, jlong data) {
 	java_project* project = reinterpret_cast<java_project*>(data);
 	std::string name = project->nameProject.string();
 	return env->NewStringUTF(name.c_str());
