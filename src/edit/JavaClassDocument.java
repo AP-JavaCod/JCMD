c94e6c8648d6b3bc210703707c963843f3c9b618
AP-JavaCod <megafoh3g@gmail.com>
2025-07-13 10:57:38 +0500

    Create JavaClassDocument.java

---
+++ src/edit/JavaClassDocument.java
@@ -0,0 +1,106 @@
+package edit;
+
+import data.FileManager;
+import edit.type.TypeJavaClass;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import java.io.File;
+
+public class JavaClassDocument extends DefaultStyledDocument {
+
+    private File fileClass;
+    private String name;
+    private String oldText;
+    private boolean isMainClass;
+    TypeJavaClass typeJavaClass;
+
+    public JavaClassDocument(File file){
+        fileClass = file;
+        name = file.getName().split("\\.")[0];
+        oldText = FileManager.reader(file);
+        isMainClass = setMainClass();
+        try {
+            super.insertString(0, oldText, null);
+        }catch (BadLocationException e){
+            oldText = "";
+        }
+        for (TypeJavaClass c : TypeJavaClass.values()){
+            String type = c.NAME + " " + name;
+            if (oldText.contains(type)){
+                typeJavaClass = c;
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+        String text = switch (str){
+            case "{" -> "{\n" + getTabs(getTabPosition(offs)) + "}";
+            case "\n" -> "\n" + getTabs(getTabPosition(offs));
+            case "(" -> "()";
+            case "\"" -> "\"\"";
+            case "\'" -> "\'\'";
+            default -> str;
+        };
+        super.insertString(offs, text, a);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMainClass() {
+        return isMainClass;
+    }
+
+    public TypeJavaClass getTypeJavaClass() {
+        return typeJavaClass;
+    }
+
+    public boolean isChanges(){
+        try {
+            return !oldText.equals(getText(0, getLength()));
+        } catch (BadLocationException e) {
+            return false;
+        }
+    }
+
+    public boolean save(){
+        try {
+            oldText = getText(0, getLength());
+            FileManager.write(fileClass, oldText);
+            return true;
+        }catch (BadLocationException e){
+            return false;
+        }
+    }
+
+    private boolean setMainClass(){
+        return oldText.contains("public static void main(String[]");
+    }
+
+    private int getTabPosition(int end){
+        try {
+            int tab = 0;
+            String text = getText(0, end).substring(0, end);
+            for (char el : text.toCharArray()) {
+                if (el == '{') {
+                    tab++;
+                } else if (el == '}') {
+                    tab--;
+                }
+            }
+            return tab;
+        } catch (BadLocationException e) {
+            return 0;
+        }
+    }
+
+    private String getTabs(int n){
+        return "\t".repeat(Math.max(0, n));
+    }
+
+}
