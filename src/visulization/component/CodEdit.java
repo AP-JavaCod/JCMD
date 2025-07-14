0839a6f32a0f242240c217aa5f8d69c45bf0e049
AP-JavaCod <megafoh3g@gmail.com>
2025-07-14 10:36:40 +0500

    Update Exception

--- src/visulization/component/CodEdit.java
+++ src/visulization/component/CodEdit.java
@@ -7,13 +7,10 @@ import javax.swing.*;
 
 public class CodEdit extends JTextArea implements Observer {
 
-    public CodEdit(){
-        setTabSize(5);
-    }
-
     @Override
     public void upData() {
         setDocument(MenuFrame.project.getActiveClass());
+        setTabSize(5);
     }
 
 }
