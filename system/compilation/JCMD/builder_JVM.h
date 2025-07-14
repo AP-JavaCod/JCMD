5a88b7c415493053ca1cecafadfadc8077a933d6
AP-JavaCod <megafoh3g@gmail.com>
2025-07-14 10:20:17 +0500

    Update Exception

--- system/compilation/JCMD/builder_JVM.h
+++ system/compilation/JCMD/builder_JVM.h
@@ -5,23 +5,22 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+	JNIEXPORT void JNICALL Java_build_Builder_console(JNIEnv*, jobject, jstring);
 	
 	JNIEXPORT jlong JNICALL Java_build_Builder_setData(JNIEnv*, jobject, jstring);
 
 	JNIEXPORT void JNICALL Java_build_Builder_deleteData(JNIEnv*, jobject, jlong);
 
-	JNIEXPORT jstring JNICALL Java_build_Builder_compilation(JNIEnv*, jobject, jlong, jstring, jdouble);
+	JNIEXPORT void JNICALL Java_build_Builder_compilation(JNIEnv*, jobject, jlong, jstring, jdouble);
 
-	JNIEXPORT jstring JNICALL Java_build_Builder_build(JNIEnv*, jobject, jlong, jstring, jstring, jdouble);
+	JNIEXPORT void JNICALL Java_build_Builder_build(JNIEnv*, jobject, jlong, jstring, jstring, jdouble);
 
 	JNIEXPORT jstring JNICALL Java_build_Builder_run(JNIEnv*, jobject, jlong, jstring, jstring, jdouble);
 
 	JNIEXPORT jint JNICALL Java_build_Builder_sizeClasses(JNIEnv*, jobject, jlong);
 
 	JNIEXPORT jstring JNICALL Java_build_Builder_getClassFile(JNIEnv*, jobject, jlong, jint);
-
-	//Deprecated
-	JNIEXPORT jstring JNICALL Java_build_Builder_getClassFormat(JNIEnv*, jobject, jlong, jint);
 
 	JNIEXPORT jstring JNICALL Java_build_Builder_getPathClass(JNIEnv*, jobject, jlong, jstring);
 
